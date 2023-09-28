d3a98205284206971f639f062df05f85a13127e7
loorenhao <loorenhao19@gmail.com>
2023-09-28 18:55:15 +0800

    Update StaffMenu.java

--- oopt/src/oopt/StaffMenu.java
+++ oopt/src/oopt/StaffMenu.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * @author User
  */
 public class StaffMenu {
-    private static final Scanner sc = new Scanner(System.in);
+
     
     public static void staffMenu() {
          int staffMenuSelect;
